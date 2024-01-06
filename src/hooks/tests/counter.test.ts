97776affd7e40abd9717554c8110dd259b75b5ee
juliusyam <juliusworldwide@gmail.com>
2024-01-06 18:49:48 +0000

    Minor compiler updates

--- src/hooks/tests/counter.test.ts
+++ src/hooks/tests/counter.test.ts
@@ -6,10 +6,20 @@ describe('useCounter', () => {
 
   it('setups initially correctly', () => {
 
-    const { result } = renderHook(useCounter);
+    const { result: resultWithDefaultInitial } = renderHook(useCounter);
 
-    expect(result.current[0]).toBe(0);
+    expect(resultWithDefaultInitial.current[0]).toBe(0);
+
+    const { result: resultWithCustomInitial } = renderHook(() => useCounter(11));
+
+    expect(resultWithCustomInitial.current[0]).toBe(11);
 
   });
 
+  // Increment Test
+
+  // Decrement Test
+
+  // Reset Test
+  // both with default initial value and with custom initial value
 });
